5d278805ce10b3ee13adf6bd8549dc38016b3c14
darkelle <djilani.cardineau@epitech.eu>
2022-05-12 22:09:42 +0200

    start

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'epitech-serverless',
+    appId: '1:711705843792:web:2692726515f46e06cbd955',
+    storageBucket: 'epitech-serverless.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAJOqxGKXYqJcJMEPAsUuVJx3BMEWW-73k',
+    authDomain: 'epitech-serverless.firebaseapp.com',
+    messagingSenderId: '711705843792',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
